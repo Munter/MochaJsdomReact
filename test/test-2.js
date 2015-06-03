de98c874fece678069a3f565f5ca6ba2f6240bd1
Peter Müller <munter@fumle.dk>
2015-06-03 16:42:41 +0200

    Add crazy failing test

---
+++ test/test-2.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var assert = require("assert");
+var jsdomify, React, Leaker;
+
+describe('DOM Leak 2', function () {
+
+  before(function () {
+    jsdomify = require('jsdomify');
+    jsdomify.create();
+
+    React = require('react');
+
+    Leaker = React.createClass({
+      render() {
+        return (
+          <div>
+            When this HTML innerText is here, the tests fail
+            {this.state ? 'foo' : null}
+          </div>
+        )
+      }
+    });
+  });
+
+  after(function () {
+    jsdomify.destroy();
+  });
+
+  it('should be defined', function () {
+    var instance = React.render(
+      <Leaker isVisible={false}></Leaker>,
+      document.body
+    );
+
+    instance.setState({isVisible: true});
+
+    assert(instance);
+  });
+});
